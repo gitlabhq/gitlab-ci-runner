65088429c096042a0bec34d2ac9f961d6f73ef63
Wachiwi <speichermail2@aol.com>
2013-05-30 14:46:09 +0300

    Core runner functionality

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source "http://rubygems.org"
 
+gem 'childprocess', '0.3.6'
+gem 'charlock_holmes', '0.6.9.4'
+
 group :development do
   gem 'coveralls', require: false
   gem 'rspec'
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,9 +1,17 @@
+require_relative 'build'
+
 module GitlabCi
   class Runner
-    def initialize
-      loop do
+    attr_accessor :current_build, :token, :thread
 
-        if running?
+    def initialize
+      puts '* Gitlab CI Runner started'
+      puts '* Waiting for builds'
+
+      loop do
+        if completed?
+          submit_build
+        elsif running?
           send_trace
         else
           get_build
@@ -13,16 +21,69 @@ module GitlabCi
       end
     end
 
+    def completed?
+      @current_build && @current_build.completed?
+    end
+
     def running?
-      # check if build running
+      @current_build
     end
 
     def send_trace
-      # send build trace to coordinator over network
+      puts collect_trace
+    end
+
+    def submit_build
+      send_trace
+      puts "Completed build #{@current_build.id}"
+      puts "Submiting build #{@current_build.id} to coordinator..."
+      @current_build = nil
     end
 
     def get_build
+      build_data = get_pending_build
+
+      if build_data
+        run(build_data)
+      else
+        false
+      end
+    end
+
+    def token
+      @token ||= File.read(File.expand_path(File.join(File.dirname(__FILE__), 'support', "token")))
+    end
+
+    def get_pending_build
       # check for available build from coordinator
+      # and pick a pending one
+      {
+        commands: ['ls -la'],
+        path: '/home/git/testproject',
+        ref: '3ee2b0fda79b326692135f5fb69da8c2eb557709',
+        id: 657
+      }
+    end
+
+    def run(build_data)
+      @current_build = GitlabCi::Build.new(
+        build_data[:id],
+        build_data[:commands],
+        build_data[:path],
+        build_data[:ref],
+      )
+
+      Thread.abort_on_exception = true
+
+      Thread.new(@current_build) do
+        puts "Build #{@current_build.id} started.."
+
+        @current_build.run
+      end
+    end
+
+    def collect_trace
+      @current_build.trace
     end
   end
 end
