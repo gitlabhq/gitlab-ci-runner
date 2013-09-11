a5fee9a374941ad9198a123e7c6f704faa29c61c
Sytse Sijbrandij <sytse@dosire.com>
2013-09-11 18:09:27 +0200

    Better debug info.

--- lib/setup.rb
+++ lib/setup.rb
@@ -36,7 +36,7 @@ module GitlabCi
           token = gets.chomp
         end
 
-        raise "GOT THERE with #{public_key}, #{token}, config.url."
+        raise "Registering runner with public key: #{public_key[0..7]}, token: #{token}, url: Config.new.url."
         runner = Network.new.register_runner(public_key, token)
 
         if runner
