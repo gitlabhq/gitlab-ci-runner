328dcb4d852f0c337cb67e9b016857be8b642170
Antonio Lobato <antonio@whisper.sh>
2014-07-11 12:53:20 -0700

    Exposed project id and project directory to build scripts.

--- lib/build.rb
+++ lib/build.rb
@@ -143,6 +143,9 @@ module GitlabCi
       @process.environment['CI_BUILD_REF_NAME'] = @ref_name
       @process.environment['CI_BUILD_ID'] = @id
 
+      @process.environment['CI_PROJECT_ID'] = @project_id
+      @process.environment['CI_PROJECT_DIR'] = project_dir
+
       @process.start
 
       @tmp_file_path = @tmp_file.path
