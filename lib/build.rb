49d7dea5a54a1b76a345bab9c8959e0136e0542a
Andrew A Smith <andy@tinnedfruit.org>
2014-10-14 09:09:19 -0700

    Expose project ID to the runner.

--- lib/build.rb
+++ lib/build.rb
@@ -142,6 +142,7 @@ module GitlabCi
       @process.environment['CI_BUILD_BEFORE_SHA'] = @before_sha
       @process.environment['CI_BUILD_REF_NAME'] = @ref_name
       @process.environment['CI_BUILD_ID'] = @id
+      @process.environment['CI_PROJECT_ID'] = @project_id
 
       @process.start
 
